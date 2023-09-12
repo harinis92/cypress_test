daea19f9510f7872ed92c02999bc0860c97846d6
Harini S <harinis.uk@gmail.com>
2023-09-12 17:01:00 +0100

    updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,21 +2,21 @@ const { defineConfig } = require("cypress");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
-on("before:run", async (details) => {
-  await beforeRunHandler(config);
-
-  // Your own `before:run` code goes here.
-});
-
-on("after:run", async (results) => {
-  await afterRunHandler(config);
-
-  // Your own `after:run` code goes here.
-});
-
 async function setupNodeEvents(on, config) {
   // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("before:run", async (details) => {
+    await beforeRunHandler(config);
+
+    // Your own `before:run` code goes here.
+  });
+
+  on("after:run", async (results) => {
+    await afterRunHandler(config);
+
+    // Your own `after:run` code goes here.
+  });
 
   on("file:preprocessor", browserify.default(config));
 
